d3b889b64c1bf04a70f9e0d4996e4126bd0bb487
Andy Ye <ye.andyy@gmail.com>
2021-06-08 13:21:07 -0400

    Bug fixes, function consolidation

--- script.js
+++ script.js
@@ -1,96 +1,107 @@
+//. for class, # for id
+const container = document.querySelector('#container');
+const colorButtons = document.querySelectorAll('.color');
+let color = 'white'; //colors are all lowercase
+
+colorButtons.forEach(button => button.addEventListener('click', changeColor));
+
+
+createGrid(16);
+
 function createGrid(sideLength) {
-    const div = document.querySelector('.grid-container');
-    let sides = "";
+    let sides;
     if (sideLength==1){ //specific to 1;
         sides = "1fr";
     }
     else {
         sides = "1fr " + 'repeat(' + (sideLength-1) + ", 1fr)";
     }
-    div.style.gridTemplateColumns = sides;
-    div.style.gridTemplateRows = sides;
+
+    container.style.gridTemplateColumns = sides; 
+    container.style.gridTemplateRows = sides;
+
+    //loop to create div nodes. Each has a unique ID, class of grid-item. Background set to white.
     for (let i = 1; i <=sideLength**2; i++){
-        const divChild = document.createElement('div');
-        divChild.classList.add('grid-item');
-        divChild.setAttribute('id', 'grid-item-' + i);
-        divChild.setAttribute('style', 'background-color: rgb(255,255,255');
-        div.append(divChild);
+        const div = document.createElement('div');
+        div.classList.add('grid-item');
+        div.setAttribute('id', 'grid-item-' + i);
+        div.setAttribute('style', 'background-color: rgb(255,255,255');
+        container.append(div);
     }
+
+    let nodes = document.querySelectorAll('.grid-item');
+    nodes.forEach((element) => element.addEventListener('mouseenter', setColorEffect));
 }
 
 function clearGrid(){
-    const hover = document.querySelectorAll('.grid-item');
-    hover.forEach((node) => node.style.background = 'rgb(255,255,255');
+    const nodes = document.querySelectorAll('.grid-item');
+    nodes.forEach((node) => node.style.background = 'rgb(255,255,255');
 }
 
-const enableWhite = function(node){
-    node.setAttribute('style', 'background-color: rgb(255,255,255');
-}
-
-const enableRainbow = function(node){
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    node.setAttribute('style', 'background-color: ' + bgColor);
-}
-
-const enableGrayScale = function(node){
-    /*
-    let currentColor = node.style.backgroundColor;
-    currentColor = currentColor.split(' ');
-    let re = /[0-9]+/g
-    let x = Math.floor(parseInt(currentColor[0].match(re)) * 0.9);
-    let y = Math.floor(parseInt(currentColor[1].match(re)) * 0.9);
-    let z = Math.floor(parseInt(currentColor[2].match(re)) * 0.9);
-    console.log(x, y, z);
-    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    node.setAttribute('style', 'background-color: ' + bgColor);
-    */
-    node.setAttribute('style', 'background-color: gray');
-}
-
-function removeHandler(currentClass){
-    if (currentClass==='rainbow'){
-        hover.forEach((node) => {
-            node.removeEventListener('mouseenter', () => enableRainbow(node));
-        });
+function setColorEffect(){ //consolidate all color options instead of individual functions
+    if (color==='white'){
+        this.setAttribute('style', 'background-color: rgb(255,255,255');
+        toggleClass(color);
     }
-    else if(currentClass==='gray'){
-        hover.forEach((node) => {
-            node.removeEventListener('mouseenter', () => enableGrayScale(node));
-        });
+    else if(color==='black'){
+        this.setAttribute('style', 'background-color: rgb(0,0,0)');
+        toggleClass(color);
     }
-    else if(currentClass==='white'){
-        hover.forEach((node) => {
-            node.removeEventListener('mouseenter', () => enableWhite(node));
-        });
+    else if(color==='rainbow'){
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+        let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        this.setAttribute('style', 'background-color: ' + bgColor);
+        toggleClass(color);
+    }
+    else if (color==='grayscale'){
+        let currentColor = this.style.backgroundColor;
+        currentColor = currentColor.split(' ');
+        let re = /[0-9]+/g
+        let x = Math.floor(parseInt(currentColor[0].match(re)) * 0.9);
+        let y = Math.floor(parseInt(currentColor[1].match(re)) * 0.9);
+        let z = Math.floor(parseInt(currentColor[2].match(re)) * 0.9);
+        //console.log(x, y, z); //color check
+        let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        this.setAttribute('style', 'background-color: ' + bgColor);
+    }
+    else {
+        console.log('fatal error');
+        this.setAttribute('style', 'background-color: pink');
+        toggleClass(color);
     }
 }
 
-function toggleClass(classToCheck){
-    const hover = document.querySelectorAll('.grid-item');
-    hover.forEach((node) => {
-        node.classList.toggle(classToCheck);
-    });
+function changeColor(event){
+    console.log(event.target.id);
+    if (color===event.target.id){
+        color = 'white';
+    }
+    else if (event.target.id==='rainbow'){
+        color = 'rainbow';
+    }
+    else if (event.target.id==='grayscale'){
+        color = 'grayscale';
+    }
+    else if (event.target.id==='black'){
+        color = 'black';
+    }
+    else {
+        color = 'white';
+    }
 }
 
+function toggleClass(classCheck){
+    container.classList.toggle(classCheck);
+}
 
+colorButtons.forEach(button => button.addEventListener('click', changeColor));
 
-createGrid(16, '.grid-container');
-
-const hover = document.querySelectorAll('.grid-item');
-let currentClass;
-hover.forEach((node) => {
-    node.addEventListener('mouseover', () => enableWhite(node));
-    node.classList.toggle('white');
-    currentClass = 'white';
-});
-
-document.getElementById('btns').addEventListener('click', function(event) {
+document.getElementById('clear').addEventListener('click',(event) => {
     if (event.target.id==='clear') {
         clearGrid();
-        let gridSize = "";
+        let gridSize;
         while(true){
         gridSize = prompt("Please enter a new total grid side size: (max 10)");
         if (parseInt(gridSize)!=gridSize){//error check for non integers, can't use strict equality
@@ -107,52 +118,7 @@ document.getElementById('btns').addEventListener('click', function(event) {
         }
         }
         clearGrid();
-        createGrid(gridSize, '.grid-container');
-    }
-    else if(event.target.id==='gray'){
-        //when i press the gray button
-        //if its already grayscale, i want to toggle off grayscale class, toggle on default (white)
-        if (currentClass===event.target.id){
-            toggleClass(event.target.id);
-            toggleClass('white');
-            //removeHandler(event.target.id);
-            hover.forEach((node) => {
-                node.addEventListener('mouseover', () => enableWhite(node));
-                currentClass = 'white';
-            });
-        }
-        //if its not grayscale, i want to toggle off current class and turn on grayscale;
-        else {
-            toggleClass(currentClass);
-            toggleClass(event.target.id);
-            //removeHandler(currentClass);
-            hover.forEach((node) => {
-                node.addEventListener('mouseover', () => enableGrayScale(node));
-                currentClass = 'gray';
-            });
-        }
-    }
-    else if(event.target.id==='rainbow'){
-        //when i press the rainbow button
-        //already rainbow, turn it off and turn on white
-        if (currentClass===event.target.id){
-            toggleClass(event.target.id);
-            toggleClass('white');
-            //removeHandler(event.target.id);
-            hover.forEach((node) => {
-                node.addEventListener('mouseover', () => enableWhite(node));
-                currentClass = 'white';
-            });
-        }
-        //if its not rainbow, i want to toggle off current and turn on rainbow;
-        else {
-            toggleClass(currentClass);
-            toggleClass(event.target.id);
-            //removeHandler(currentClass);
-            hover.forEach((node) => {
-                node.addEventListener('mouseover', () => enableRainbow(node));
-                currentClass = 'rainbow';
-            });
-        }   
+        createGrid(gridSize);
     }
 });
+
